15d634fcba2cb9442a62c9fd1f305dcb27ae6c38
Stiven Gomez <stivengomez@soyyo.co>
2023-01-25 15:12:16 -0500

    Add components and layout.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
