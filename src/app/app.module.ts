f64dc8d029402e0b33d7fbf8e5a0e67022d0cd98
Stiven Gomez <stivengomez@soyyo.co>
2023-01-25 18:04:17 -0500

    Add component contact.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { LayoutComponent } from './layout/layout.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LayoutComponent
+    LayoutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
