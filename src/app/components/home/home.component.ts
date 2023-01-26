966d59659d2df1c67c461ff95f8e200b198d2524
Stiven Gomez <stivengomez@soyyo.co>
2023-01-25 22:54:05 -0500

    Fix design and data.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ProfileModel } from 'src/app/core/models/profile_model';
+import { GetProfileService } from '../../core/services/get-profile.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  isLoading = true;
+  response: ProfileModel = {};
+  constructor(private getProfileService: GetProfileService) { }
+  
+  ngOnInit() {
+    this.getProfileService.getProfile().subscribe(data => {
+      this.isLoading = false;
+      this.response = data
+    });
+  }
 
 }
+
