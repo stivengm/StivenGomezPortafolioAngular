44d13f2d0c8b94fa202089dff4ad7ae20a7a3508
Stiven Gomez <stivengomez@soyyo.co>
2023-01-26 22:50:43 -0500

    FIX - Fix contact from service.

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProfileModel } from 'src/app/core/models/profile_model';
+import { ChangeDataService } from 'src/app/core/services/change-data.service';
+import { GetProfileService } from 'src/app/core/services/get-profile.service';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+  response: ProfileModel = {};
+
+  constructor(public dataService: ChangeDataService, private getProfileService: GetProfileService) { }
+
+  ngOnInit() {
+    if (!this.dataService.profileInformation) {
+      this.dataService.isLoading.emit(true);
+      this.getProfileService.getProfile().subscribe(data => {
+        this.dataService.isLoading.emit(false);
+        this.dataService.profileInformation = data;
+      });
+    }
+  }
 
 }
