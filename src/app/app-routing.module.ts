6eec120b73bcc95262152c9088e284bd8e6d77da
Stiven Gomez <stivengomez@soyyo.co>
2023-01-26 22:55:07 -0500

    FIX - Fix router aboutme.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutMeComponent } from './components/about-me/about-me.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { HomeComponent } from './components/home/home.component';
 import { ProjectsComponent } from './components/projects/projects.component';
@@ -21,6 +22,10 @@ const routes: Routes = [
   {
     path: 'projects',
     component: ProjectsComponent,
+  },
+  {
+    path: 'about-me',
+    component: AboutMeComponent,
   },
   {
     path: '**',
