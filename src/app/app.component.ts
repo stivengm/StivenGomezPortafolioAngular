ae8172aee8656e9965da4c49210acf8f36b6b089
Stiven Gomez <stivengomez@soyyo.co>
2023-01-26 22:51:56 -0500

    FIX - Fix appComponent emit information service.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ChangeDataService } from './core/services/change-data.service';
+import { GetProfileService } from './core/services/get-profile.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'stiven_gomez_portfolio';
   isLoading: boolean = true;
-  constructor(public dataService: ChangeDataService) { }
+  constructor(private getProfileService: GetProfileService, public dataService: ChangeDataService) { }
 
   ngOnInit() {
     this.dataService.isLoading.subscribe(data => this.isLoading = data);
+
+    this.getProfileService.getProfile().subscribe(data => {
+      this.dataService.isLoading.emit(false);
+      this.dataService.profileInformation = data;
+    });
   }
 }
