8e8e7890d76b82dadff3f5c69aa22c59e5de8e15
Stiven Gomez <stivengomez@soyyo.co>
2023-01-26 22:53:05 -0500

    FIX - contact information empy.

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ChangeDataService } from 'src/app/core/services/change-data.service';
+import { GetProfileService } from 'src/app/core/services/get-profile.service';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss']
 })
-export class ProjectsComponent {
+export class ProjectsComponent implements OnInit {
+
+  constructor(private dataService: ChangeDataService, private getProfileService: GetProfileService) { }
+
+  ngOnInit() {
+    if (!this.dataService.profileInformation) {
+      this.dataService.isLoading.emit(true);
+      this.getProfileService.getProfile().subscribe(data => {
+        this.dataService.isLoading.emit(false);
+        this.dataService.profileInformation = data;
+      });
+    }
+  }
 
 }
