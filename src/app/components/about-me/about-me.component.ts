a3e76d716809ea8415ff02c7096d4671ad5c21b2
Stiven Gomez <stivengomez@soyyo.co>
2023-01-26 22:58:50 -0500

    FIX - Add service and information.

--- src/app/components/about-me/about-me.component.ts
+++ src/app/components/about-me/about-me.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ChangeDataService } from 'src/app/core/services/change-data.service';
+import { GetProfileService } from 'src/app/core/services/get-profile.service';
 
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
   styleUrls: ['./about-me.component.scss']
 })
-export class AboutMeComponent {
+export class AboutMeComponent implements OnInit {
+
+  constructor( public dataService: ChangeDataService, private getProfileService: GetProfileService ) {}
+
+  ngOnInit() {
+    if (!this.dataService.profileInformation) {
+      this.dataService.isLoading.emit(true);
+      this.getProfileService.getProfile().subscribe(data => {
+        this.dataService.isLoading.emit(false);
+        this.dataService.profileInformation = data;
+      });
+    }
+  }
 
 }
