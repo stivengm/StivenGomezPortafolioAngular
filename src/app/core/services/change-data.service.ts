75ed4a4ba4ec4de4ef0bc23b0ba34fe85f2ad814
Stiven Gomez <stivengomez@soyyo.co>
2023-01-26 22:52:24 -0500

    FIX - Fix dataService.

--- src/app/core/services/change-data.service.ts
+++ src/app/core/services/change-data.service.ts
@@ -1,10 +1,13 @@
 import { Injectable, EventEmitter } from '@angular/core';
+import { ProfileModel } from '../models/profile_model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChangeDataService {
-  profileInformation$ = new EventEmitter<boolean>();
+
+  profileInformation: ProfileModel = {};
+
   isLoading = new EventEmitter<boolean>();
   constructor() { }
 }
